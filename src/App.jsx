6a0cbb4f45b31d2e6691371b22bf89184baab78e
Arunav Singh <arunavsingh999@gmail.com>
2025-06-13 21:35:44 +0530

    heading completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,61 @@
 const App = () => {
   return (
     <>
-      <div className="">Hello</div>
+      <div className="container">
+        {/* Heading of the cart */}
+        <div className="heading">
+          <div className="icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
+              />
+            </svg>
+          </div>
+          <div className="head-name">
+            <h2>Items Cart</h2>
+          </div>
+          <div className="icon cart-helper">
+            <span className="cart-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                />
+              </svg>
+            </span>
+            <span className="cart-num">3</span>
+          </div>
+        </div>
+
+        {/*  */}
+        <div className="cart-items"></div>
+
+        {/*  */}
+        <div className="promo"></div>
+
+        {/*  */}
+        <div className="total"></div>
+
+        {/*  */}
+        <div className="payment"></div>
+      </div>
     </>
   );
 };
